aa81ce652e1c17576eb3629de748630ea951c271
mHALr <ryo97321@gmail.com>
2020-01-25 11:16:35 +0900

    add class-and-style/

---
+++ tutorial/class-and-style/index.js
@@ -0,0 +1,10 @@
+var vm = new Vue({
+    el: '#example',
+    data: {
+        message: 'Test Message.',
+        styleObject: {
+            color: 'red',
+            fontSize: '30px'
+        }
+    }
+})
